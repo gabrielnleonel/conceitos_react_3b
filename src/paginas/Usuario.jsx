7353a904fd5582b78384cde9a3ba1105c4857085
GABRIEL NICOLLI LOZANO LEONEL <gabriel_n_leonel@estudante.sesisenai.org.br>
2026-05-14 11:57:29 -0300

    commit

--- src/paginas/Usuario.jsx
+++ src/paginas/Usuario.jsx
@@ -1,11 +1,19 @@
 import { useEffect, useState } from "react"
 
-
 export default function Usuario () {
     const [contador, setContador] = useState(0)
+    const [usuarios, setUsuarios] = useState(0)
 
     useEffect( () => {
         document.title = 'contagem ' + contador
+        const buscarUsuarios = async () => {
+            const resposta = await fetch('http://localhost:3000/usuarios')
+            const usuarios = await resposta.json()
+            setUsuarios(data)
+
+            console.log(data);
+        }
+
     }, [contador])
 
     return (
@@ -14,6 +22,15 @@ export default function Usuario () {
             <h1>{contador}</h1>
 
             <button onClick={() => {setContador(contador + 1)}}>somar</button>
+            
+            <ul>
+            {usuarios.map( (usuario) => {
+                <li key = {usuario.id}>
+                    <b> {usuario.nome} </b>
+                    STATUS: { usuario.ativo ? 'Ativo' : 'Desativo'}
+                </li>
+            })}
+            </ul>
         </div>
    
     )
